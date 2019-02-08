fec44f96dc8bb667718b54a310d26a562b96d174
Alfian Losari <alfianlosari@gmail.com>
2019-02-08 19:20:44 +0700

    Initial Commit

---
+++ CollectionViewResponsiveLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewResponsiveLayout
+//
+//  Created by Alfian Losari on 2/8/19.
+//  Copyright © 2019 Alfian Losari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
